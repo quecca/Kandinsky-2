f874a6020d19e46b8b01a1b7efc128685e9f07fc
Ivaniv Anton <32462360+IvanivAnton@users.noreply.github.com>
2023-05-24 17:07:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "regex",
         "numpy",
         "blobfile",
-        "transformers==4.23.1",
+        #"transformers==4.23.1",
         "torchvision",
         "omegaconf",
         "pytorch_lightning",
